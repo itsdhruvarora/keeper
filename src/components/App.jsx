e1564be1b1bcddcb1bda7f57921e53cdfc0a59d2
Dhruv <dhruv2073.be21@chitkara.edu.in>
2023-07-16 17:46:51 +0530

    completed part 2 of the app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+function createNotes(noteitem){
+  return (
+    <Note 
+    title = {noteitem.title}
+    content = {noteitem.content}
+    /> 
+  );
+}
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(createNotes)}
       <Footer />
     </div>
   );
