fd2fda4ce97348f964c25723ea4b9b4acd1f308c
Dhruv <dhruv2073.be21@chitkara.edu.in>
2023-07-31 14:49:24 +0530

    completed the keeper app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
-
-function createNotes(noteitem){
-  return (
-    <Note 
-    title = {noteitem.title}
-    content = {noteitem.content}
-    /> 
-  );
-}
+import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, { ...newNote, id: uuidv4() }];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem) => noteItem.id !== id);
+    });
+  }
+
   return (
     <div>
       <Header />
-      {notes.map(createNotes)}
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem) => {
+        return (
+          <Note
+            key={noteItem.id}
+            id={noteItem.id}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
